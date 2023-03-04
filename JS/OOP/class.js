2f39c8ef1300ccfd49d49efa5d4a8e16ccba04e5
Arteks718 <artexbro.official@gmail.com>
2023-03-04 13:14:59 +0200

    наследование

--- JS/OOP/class.js
+++ JS/OOP/class.js
@@ -1,56 +1,90 @@
-let users = [];
+// let users = [];
 
+// class User{
+//     constructor(nameValue = "anov", surnameValue = "anov"){
+//         this.name = nameValue;
+//         this.surname = surnameValue;
+//     }
+// }
+
+// let user1 = new User("Tom", "Fox");
+
+// console.log(user1);
+
+// let user2 = new User("Ann", "Smith");
+
+// console.log(user2);
+
+// for (let i = 0; i < 20; i++) { 
+//     users[i] = new User(`name${i}`, `surname${i}`)
+// }
+
+// const user3 = new User();
+
+// class Engine{
+//     constructor(name, power){
+//         this.name = name;
+//         this.power = power;
+//     }
+// }
+
+// class Car{
+//     constructor(color, year, model, engine){
+//         this.color = color;
+//         this.year = year;
+//         this.model = model;
+//         this.engine = engine;
+//     }
+//     printObject(){
+//         console.log(this);
+//     }
+// }
+
+// let engine1 = new Engine('2JZ', 1.9)
+
+// let car1 = new Car("Gray", 2004, "Skoda", engine1)
+// car1.printObject();
+
+// class Rect{
+//     constructor(a, b){
+//         this.a = a;
+//         this.b = b;
+//         this.square = 0;
+//     }
+//     calcSquare(){
+//         this.square = this.a * this.b;
+//         return this.square;
+//     }
+//     outSquare(){
+//         console.log(this.square);
+//     }
+// }
+
+// let r1 = new Rect(20,4)
+
+//Наследование
 class User{
-    constructor(nameValue = "anov", surnameValue = "anov"){
-        this.name = nameValue;
-        this.surname = surnameValue;
+    #login;
+    #password;
+    constructor(login, password, fullname="anov"){
+        this.#login = login
+        this.#password = password;
+        this.fullname = fullname;
+    }
+    set login(value){
+        this.login = value
+    }
+    set password(value){
+        this.password = value
+    }
+    get login(){
+        return this.login
+    }
+    get password(){
+        return this.password
     }
 }
 
-let user1 = new User("Tom", "Fox");
+let user1 = new User("Tom", "fghfhg")
 
 console.log(user1);
-
-let user2 = new User("Ann", "Smith");
-
-console.log(user2);
-
-for (let i = 0; i < 20; i++) { 
-    users[i] = new User(`name${i}`, `surname${i}`)
-}
-
-const user3 = new User();
-
-class Car{
-    constructor(color, year, brand, model, engineCapacity){
-        this.color = color;
-        this.year = year;
-        this.brand = brand;
-        this.model = model;
-        this.engineCapacity = engineCapacity;
-    }
-    printObject(){
-        console.log(this);
-    }
-}
-
-let car1 = new Car("Gray", 2004, "Skoda", "Octavia", 2.3)
-car1.printObject();
-
-class Rect{
-    constructor(a, b){
-        this.a = a;
-        this.b = b;
-    }
-    calcSquare(){
-        this.square = this.a * this.b;
-        return this.square;
-    }
-    outSquare(){
-        console.log(this.square);
-    }
-}
-
-let r1 = new Rect(20,4)
-
-console.log(r1.calcSquare());
