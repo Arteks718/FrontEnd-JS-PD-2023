8e70ce8be5ff5105d7d82474b050a072f0c3dbbb
Arteks718 <artexbro.official@gmail.com>
2023-03-22 17:43:29 +0200

    separate branch HTML-CSS

---
+++ DOM/assets/js/index.js
@@ -0,0 +1,11 @@
+// const items = document.querySelectorAll('.item');
+// const colors = ['red', 'pink', 'green', 'purple', 'yellow']
+
+// items.forEach((i, index)=>{
+//   i.addEventListener('click',changeSizeItem(index))
+
+// })
+
+// function changeSizeItem(index){
+//   document.body.style.backgroundColor = colors[index];
+// }
